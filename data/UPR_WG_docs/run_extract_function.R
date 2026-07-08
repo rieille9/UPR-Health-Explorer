58778330ebfffd47bdb0c39f3be9977437521f82
Anshu Uppal <49289799+anshu-uppal@users.noreply.github.com>
2026-07-08 13:51:32 +0200

    fixed so that Central African Republic session 5 recommendations are extracted as `provisional = TRUE`, and regenerated the `recs_combined.rds` file as well as the `UHRI_UPR_classified.rds` files.

--- data/UPR_WG_docs/run_extract_function.R
+++ data/UPR_WG_docs/run_extract_function.R
@@ -5,15 +5,15 @@ pacman::p_load(
 
 # Run the extract function for each country as needed
 extract_upr_recs(
-  input = "https://www.ohchr.org/sites/default/files/lib-docs/HRBodies/UPR/Documents/session23/LC/UPR23_SaintLucia_recommendations.docx",
-  state_under_review = "Saint Lucia", 
-  upr_session = 23, 
-  document_symbol = "A/HRC/31/10",
-  provisional = FALSE, mode = "auto"
+  input = "https://docs.un.org/en/A/HRC/12/2",
+  state_under_review = "Central African Republic", 
+  upr_session = 5, 
+  document_symbol = "A/HRC/12/2",
+  provisional = TRUE, mode = "auto"
 )
 
 # Inspect the extracted file for any issues
-recs2 <- readRDS(here("data", "UPR_WG_docs", "extracted_recs", "Saint Lucia_23.rds"))
+recs2 <- readRDS(here("data", "UPR_WG_docs", "extracted_recs", "Central African Republic_5.rds"))
 
 
 # Uncomment below code for first run
